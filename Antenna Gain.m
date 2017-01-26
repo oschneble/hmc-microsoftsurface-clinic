afd61d7f87d38aba49d908be9823310f17c9df8b
oschneble <oschneble@users.noreply.github.com>
2017-01-25 20:04:41 -0800

    Update Antenna Gain.m

--- Antenna Gain.m
+++ Antenna Gain.m
@@ -13,21 +13,22 @@ VSWR = 1.3;
 Rec_P = -32.5;
 
 %CALCULATED VALUES:
-Trans_Loss = -10*log10(1-((VSWR-1)/(VSWR+1))^2);
+%Trans_Loss = -10*log10(1-((VSWR-1)/(VSWR+1))^2);
+MM_Loss = 10*log10(1-10^(VSWR/10));
 Wavelength = 3*10^8/Frequency;
 
 
 %OTHER VALUES: 
 %Trans_Loss = 0.18; %vswr = 1.5
 Misc_Loss = 0;
-Rec_Ant_Gain = 12.5 -2.15;%convert dbi to dbd
+Rec_Ant_Gain = 12.5 -2.15;%convert dbi to dbd, vary w/ frequency
 Rec_Loss = 0; %vswr = 1.07
 Distance = 1.778;%anechoic chamber length
 Friis_Eq = 20*log10(Wavelength/(4*pi*(Distance)));
 
 %GAIN CALCULATION:
 % Rec_P = Trans_P + Trans_Ant_Gain - Trans_Loss - Misc_Loss + Rec_Ant_Gain - Rec_Loss + Friis_Eq
-Trans_Ant_Gain = -(Trans_P - Trans_Loss ) + (Rec_P + Rec_Loss) + Misc_Loss - Rec_Ant_Gain - Friis_Eq
+Trans_Ant_Gain = -(Trans_P - Trans_Loss) + (Rec_P + Rec_Loss) + Misc_Loss - Rec_Ant_Gain - Friis_Eq
 
 %Dipole antenna 1: Trans_Ant_Gain = 3.1856 with vswr S11 = 1.6 vswr S22 =
 %1.07 Rec_P = -32.22
